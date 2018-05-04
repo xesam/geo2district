90bbb89dcc86edb1e557477be6bc39ae000668d1
xesam <xesamguo@gmail.com>
2018-05-04 17:58:39 +0800

    'prepare'

--- scripts/run.js
+++ scripts/run.js
@@ -6,5 +6,7 @@ let start_file = argv[2];
 let in_dir = argv[3];
 let out_dir = argv[4];
 
-prepare.build_skeleton(start_file);
+prepare.build_skeleton(start_file, null, (dict)=>{
+    console.log(dict[0].districts['北京市'].name);
+});
 // prepare.run(skeleton, in_dir, out_dir);
